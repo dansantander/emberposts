49989fbcbfbc948b87616ab08981e1f408898340
Daniel Santander <danyvo1d@gmail.com>
2020-10-17 13:05:49 -0500

    Add create post functionality

---
+++ app/controllers/create.js
@@ -0,0 +1,24 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+export default class CreateController extends Controller {
+  @action
+  createPost(event) { /* event is always the last argument on the action */
+    event.preventDefault();
+    //step 1: read the inputs
+    console.log(this.title, this.body)
+    //step 2: save the post
+    let post = this.store.createRecord('post', {
+      title: this.title,
+      body: this.body
+    });
+    //remember: the controller also has access to the store
+    //the first argument is the model name (like saying new instance of the post model)
+    //the second argument is the data
+    //and now we save the record to the server and redirect
+    post.save().then(()=> {
+      //this.transitionToRoute('index');
+      this.transitionToRoute('post', post.id);
+    })
+  }
+}
